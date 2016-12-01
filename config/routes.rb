c80f178dc016969a40be42c9395246ae4f6f9074
Robby <robbyd628@gmail.com>
2016-12-01 15:01:16 +0000

    heroku app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
-  root :to => redirect('/movies')
+  root :to => redirect('/dryers')
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
