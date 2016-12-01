087a272a4df39b6bd706fbdeb98b7313c7d0ad48
Robby <robbyd628@gmail.com>
2016-12-01 14:44:35 +0000

    routes.rb template added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
-Rails.application.routes.draw do
+Rottenpotatoes::Application.routes.draw do
+  resources :movies
+  # map '/' to be a redirect to '/movies'
+  root :to => redirect('/movies')
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
